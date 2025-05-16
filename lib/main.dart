0893f55c85dfcc1f5c242221edd26bbcfbb57e3c
Yaser yjjou <125563605+0yaser0@users.noreply.github.com>
2025-05-16 21:58:43 +0100

    feat : add Wifi connexion &&  send and receive data

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:safe_ride_app/presentation/widgets/main/custom_floating_button.dart';
-import 'package:safe_ride_app/presentation/widgets/main/custom_bottom_navigation_bar.dart';
-import 'package:safe_ride_app/presentation/widgets/main/show_bubble_popup.dart';
-import 'presentation/pages/home/home_page.dart';
-import 'presentation/pages/maps/maps_page.dart';
-import 'presentation/pages/history/history_page.dart';
-import 'presentation/pages/settings/settings.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'core/theme/theme.dart';
+import 'dart:io';
+
+import 'package:safe_ride_app/core/theme/theme.dart';
 
 void main() {
   runApp(const SafeRideApp());
@@ -18,101 +12,130 @@ class SafeRideApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainScreen(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("ESP32 Communicator")),
+        body: ESP32Controller(),
+      ),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+class ESP32Controller extends StatefulWidget {
+  const ESP32Controller({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  State<ESP32Controller> createState() => _ESP32ControllerState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  final PageController _pageController = PageController();
-  int _selectedIndex = 0;
-  GlobalKey iconKey = GlobalKey();
+class _ESP32ControllerState extends State<ESP32Controller> {
+  late Socket socket;
+  final ip = "192.168.4.1";
+  final port = 3333;
+  String response = "No response yet";
+  String message = "";
+  List<String> messageHistory = [];
 
-  void _onPageSelected(int index) {
-    _pageController.jumpToPage(index);
-    setState(() {
-      _selectedIndex = index;
-    });
+  Future<void> connectToESP32() async {
+    try {
+      socket = await Socket.connect(ip, port);
+      setState(() {
+        response = "Connected to ESP32!";
+      });
+
+      socket.listen(
+        (data) {
+          String receivedMessage = String.fromCharCodes(data).trim();
+
+          if (receivedMessage.isNotEmpty) {
+            setState(() {
+              response = receivedMessage;
+              messageHistory.insert(0, receivedMessage);
+            });
+          } else {
+            _showCustomSnackBar(context, "empty response");
+          }
+        },
+        onError: (error) {
+          setState(() {
+            response = "Connection error.";
+            _showCustomSnackBar(context, response);
+          });
+        },
+        onDone: () {
+          setState(() {
+            response = "Disconnected.";
+            _showCustomSnackBar(context, response);
+          });
+        },
+      );
+    } catch (e) {
+      setState(() {
+        response = "Failed to connect: $e";
+        _showCustomSnackBar(context, response);
+      });
+    }
+  }
+
+  void sendMessage() {
+    if (message.isNotEmpty) {
+      socket.write(message);
+    } else {
+      _showCustomSnackBar(context, "Message is Empty");
+    }
+  }
+
+  void _showCustomSnackBar(BuildContext context, String message) {
+    final snackBar = SnackBar(
+      content: Text(message, textAlign: TextAlign.center),
+      backgroundColor: c.darkColor,
+      behavior: SnackBarBehavior.floating,
+      elevation: 0,
+      duration: const Duration(seconds: 7),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+    );
+
+    ScaffoldMessenger.of(context)
+      ..removeCurrentMaterialBanner()
+      ..showSnackBar(snackBar);
   }
 
   @override
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: c.lightColor,
-      appBar: AppBar(
-        title: const Text.rich(
-          TextSpan(
-            children: [
-              TextSpan(
-                text: 'Safe',
-                style: TextStyle(
-                  color: c.darkColor,
-                  fontSize: 18,
-                  fontFamily: 'DM Sans',
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              TextSpan(
-                text: 'Ride',
-                style: TextStyle(
-                  color: c.primaryColor,
-                  fontSize: 18,
-                  fontFamily: 'DM Sans',
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Status: $response", style: TextStyle(fontSize: 18)),
+          SizedBox(height: 10),
+          Expanded(
+            child:
+                messageHistory.isEmpty
+                    ? Center(child: Text("No messages yet"))
+                    : ListView.builder(
+                      itemCount: messageHistory.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          color: Colors.grey[300],
+                          margin: EdgeInsets.symmetric(vertical: 4),
+                          child: ListTile(title: Text(messageHistory[index])),
+                        );
+                      },
+                    ),
           ),
-          textAlign: TextAlign.center,
-        ),
-        backgroundColor: const Color(0x00000000),
-        elevation: 0,
-        actions: [
-          Builder(
-            builder: (context) {
-              return IconButton(
-                key: iconKey,
-                icon: SvgPicture.asset(
-                  'assets/icons/notifications.svg',
-                  width: 24,
-                  height: 24,
-                ),
-                onPressed: () {
-                  showBubblePopup(context, iconKey);
-                },
-              );
-            },
-          )
+          SizedBox(height: 20),
+          TextField(
+            onChanged: (val) => message = val,
+            decoration: InputDecoration(labelText: "Enter message"),
+          ),
+          ElevatedButton(onPressed: sendMessage, child: Text("Send Message")),
+          ElevatedButton(
+            onPressed: connectToESP32,
+            child: Text("Connect to ESP32"),
+          ),
         ],
-      ),
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: (index) {
-          setState(() => _selectedIndex = index);
-        },
-        children: const [
-          HomePage(),
-          MapsPage(),
-          HistoryPage(),
-          SettingsPage(),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: CustomFloatingButton(
-        onPressed: () => _onPageSelected(0),
-      ),
-      bottomNavigationBar: CustomBottomNavigationBar(
-        selectedIndex: _selectedIndex,
-        onPageSelected: _onPageSelected,
       ),
     );
   }
